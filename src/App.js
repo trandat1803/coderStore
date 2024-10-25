5d2777a4b0a4773e1108b0bd35484892682e3252
trandat1803 <quocdat@MacprocuaDatne.local>
2024-10-25 14:48:07 +0700

    dat

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+import { Box, Link } from '@mui/material';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.coderschool.vn">
+        CoderSchool
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom align="center">
+          My new React app
+        </Typography>
+        <Copyright />
+      </Box>
+    </>
+  );
+}
+
+export default App;
